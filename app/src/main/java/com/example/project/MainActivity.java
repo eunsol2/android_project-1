9669a971936aeb3af6bce11830c8c8db2d8cec9d
iruy0326 <eulee98@gmail.com>
2019-03-13 15:28:02 +0900

    시작

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android:text="안녕하세요";
     }
+
 }
